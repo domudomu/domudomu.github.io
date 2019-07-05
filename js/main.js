52a065541a38446dd05678d59f24a97d48a11833
Dominic Fawcett <Dominic.Fawcett@jacobs.com>
2019-07-05 10:29:22 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// $(function () {
+//   $('[data-toggle="popover"]').popover()
+// })
+$(document).ready(function () {
+  $('#navbar-toggler-icon-animated').click(function () {
+      $(this).toggleClass('open')
+  })
+});
