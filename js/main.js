5bbe5ef845c5782b7734319c411b49e673e8a4b6
Dominic Fawcett <Dominic.Fawcett@jacobs.com>
2019-11-20 12:37:48 +0000

    alignit landing page first ommit

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// $(function () {
-//   $('[data-toggle="popover"]').popover()
-// })
-$(document).ready(function () {
-  $('#navbar-toggler-icon-animated').click(function () {
-      $(this).toggleClass('open')
-  })
-});
